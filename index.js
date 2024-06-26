c86ed568b6cd20659295a5e891a8d8da1b258270
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-06-26 13:07:52 +0300

    passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(42 - pickup);
+}
+function distanceFromHqInFeet(dist){
+    return (distanceFromHqInBlocks(dist) * 264);
+}
+function distanceTravelledInFeet(start , destination){
+    return Math.abs((start-destination) *264);
+}
+function calculatesFarePrice(start , destination){
+    return distanceTravelledInFeet(start ,destination) <= 400? 0 :
+        distanceTravelledInFeet(start , destination) <= 2000? (distanceTravelledInFeet(start , destination) - 400) * 0.02 :
+                distanceTravelledInFeet(start , destination) <= 2500? 25 :
+                    'cannot travel that far';
+}
